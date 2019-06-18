6284f591d62a07f1a0fcc68ec4616fc05b78ea24
Igor Samoylenko <antonych@yandex.ru>
2019-06-18 21:31:43 +0300

    Yes

--- FirstProg.py
+++ FirstProg.py
@@ -1,1 +1,11 @@
-print(ord('e'))
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+    fib(1000)
+print("Hello world!")
+a = 5
+if a == 5:
+    print('Равно 5!')
